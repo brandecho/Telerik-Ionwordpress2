cf47b77cd8898367f48cdf216f03488aeb4ff727
brandecho <brandechomedia@gmail.com>
2015-06-10 10:01:13 -0400

    Uploaded from Local Directory to Telerik

---
+++ IonWordpress2/js/config.js
@@ -0,0 +1,6 @@
+angular.module('your_app_name.config', [])
+.constant('WORDPRESS_API_URL', 'http://www.brandechomedia.com/wp/api/')
+.constant('WORDPRESS_PUSH_URL', 'http://www.brandechomedia.com/wp/api/')
+.constant('GCM_SENDER_ID', '30879657255')
+
+;
